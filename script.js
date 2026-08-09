09c2b23766c9311cc23887eafc8826bd67defa55
javier pena <123456789+odin@users.noreply.github.com>
2026-08-08 23:08:20 -0400

    creating containers on the HTML file, still incompleted

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const add = function (a, b) {
+  return a + b;
+};
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multi = function (a, b) {
+  return a * b;
+};
+
+const div = function (a, b) {
+  return a / b;
+};
+
+function operate(num1, num2) {}
