a0e33a7fe4647a08fb7ef988b9dcfa20f3a5b7d4
javier pena <123456789+odin@users.noreply.github.com>
2026-08-13 22:49:36 -0400

    UI numbers and operator aligned

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const add = function (a, b) {
-  return a + b;
+/*
+Your calculator is going to contain functions for all of the basic math operators you typically 
+find on calculators, so start by creating functions for the following items and testing them in your browser’s console:
+add
+subtract
+multiply
+divide
+
+A calculator operation will consist of a number, an operator, and another number. 
+For example, 3 + 5. Create three variables, one for each part of the operation. 
+You’ll use these variables to update your display later.
+*/
+
+const add = function (num1, operator, num2) {
+  return (operator = num1 + num2);
 };
-const subtract = function (a, b) {
-  return a - b;
+const subtract = function (num1, operator, num2) {
+  return (num1, operator, num2);
 };
 
-const multi = function (a, b) {
-  return a * b;
+const multi = function (num1, operator, num2) {
+  return (num1, operator, num2);
 };
 
-const div = function (a, b) {
-  return a / b;
+const div = function (num1, operator, num2) {
+  return (num1, operator, num2);
 };
 
-function operate(num1, num2) {}
+console.log(add(5 + 5));
+
+/* Create a new function "operate" that takes an operator 
+ and two numbers and then calls one of the above 
+ functions on the numbers.*/
+
+function operate(num1, operator, num2) {
+  const addNum = add(num1, num2, operator);
+  return num1;
+}
